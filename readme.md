f4ed849e7b9587fde056301dd0f9c294f2645b92
angel <angel.muelas@denizen.io>
2019-06-26 15:37:06 +0200

    test: added tests

--- readme.md
+++ readme.md
@@ -13,11 +13,82 @@ You can install blue-pencil using npm.
 npm install --save blue-pencil
 ```
 
-# API Reference
+# API
 
-## Modules
+Exported symbols:
 
-<dl>
-<dt><a href="#module_blue-pencil">blue-pencil</a></dt>
-<dd></dd>
-</dl>
+- [BluePencil](#markdown-header-bluePencil)
+
+**Kind**: global class  
+
+* [BluePencil](#markdown-header-bluePencil)
+    * [new BluePencil(options)](#markdown-header-new-bluePencil-options)
+    * [.censor(objectToCensor, valuesToHide)](#markdown-header-bluePencilcensor) ⇒ <code>Object</code> &#124; <code>Array</code>
+
+### new BluePencil(options)
+<p>Create an BluePencils that hide the give keys in a given object.</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>Param</th><th>Type</th><th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+<tr>
+    <td>options</td><td>object</td><td></td>
+    </tr><tr>
+    <td>options.wildcard</td><td>string</td><td><p>Wildcard we would like to use for the mask (e.g.: <code>#####</code>)</p></td>
+    </tr><tr>
+</table>
+
+<a name="module_blue-pencil"></a>
+
+**Example**  
+```javascript
+const bluePencil = require('blue-pencil');
+let myBluePencil = new BluePencil({escapeWildcard: '#####'});
+```
+
+### blue-pencil.censor(objectToCensor, valuesToHide) ⇒ <code>Object</code> &#124; <code>Array</code>
+Returns the given object but with the given values masked.
+
+**Type**: static method of <code>[blue-pencil](#module_blue-pencil)</code>  
+**Returns**: <code>Object</code> The new object with the values masked.  
+
+<table>
+  <thead>
+    <tr>
+      <th>Param</th><th>Type</th><th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+<tr>
+    <td>objectToCensor</td><td><code>Object</code> &#124; <code>Array</code></td><td>Input data to be filtered</td>
+    </tr><tr>
+    <td>valuesToHide</td><td><code>Array</code></td><td><p>Elements to mask in the given object</td>
+    </tr><tr>
+</table>
+
+**Example**  
+```javascript
+ const bluePencil = require('blue-pencil');
+
+let myBluePencil = new BluePencil({escapeWildcard: '#####'});
+
+const nestedObj = {
+  root: [{
+    foo: 'bar',
+  }],
+};
+
+const valuesToHide = ['foo'];
+
+// censor
+const filteredObject = myBluePencil.censor(nestedObj, valuesToHide)
+
+```
+
+# License
+
+MIT @ Angel Muelas Martinez
--- tests/bluePencil.test.js
+++ tests/bluePencil.test.js
@@ -2,12 +2,73 @@ const BluePencil = require('../lib/blue-pencil/BluePencil');
 
 describe('BluePencil tests', () => {
 
+  const objectToCensor = {
+    root: [{
+      foo: 'bar',
+    }],
+  };
+  const valuesToHide = ['foo'];
+
   it('should return an error if we initialize wrongly the lib', () => {
     expect(() => new BluePencil('badConfig')).toThrow(new Error('"options" must be an object'));
   });
 
-  it('should not return an error if we initialize properly the lib', () => {
-    let myBluePencil = new BluePencil({});
+  it('should return an error if we initialize properly the lib but with a not allowed options field', () => {
+    expect(() => new BluePencil({})).toThrow(new Error('"options.wildcard" must be a string'));
+  });
+
+  it('should return an error if we initialize properly the lib but with a not allowed options field', () => {
+    let myBluePencil = new BluePencil({wildcard: '#####'});
     expect(myBluePencil).toBeInstanceOf(BluePencil)
+  });
+
+  it('given an object should return expected object masked', () => {
+    let myBluePencil = new BluePencil();
+    const expectedObject = {
+      root: [{
+        foo: '******',
+      }],
+    };
+    const result = myBluePencil.censor(objectToCensor, valuesToHide);
+    expect(result).toStrictEqual(expectedObject);
+  });
+
+  it('given an object should return expected object masked with the given wildcard', () => {
+    let myBluePencil = new BluePencil({wildcard: '######'});
+    const expectedObject = {
+      root: [{
+        foo: '######',
+      }],
+    };
+    const result = myBluePencil.censor(objectToCensor, valuesToHide);
+    expect(result).toStrictEqual(expectedObject);
+  });
+
+  it('given an array of objects should return expected object masked', () => {
+    let myBluePencil = new BluePencil();
+    const expectedObject = [{
+      root: [{
+        foo: '******',
+      }],
+    }];
+    const result = myBluePencil.censor([objectToCensor], valuesToHide);
+    expect(result).toStrictEqual(expectedObject);
+  });
+
+  it('given an array of object should return expected object masked with the given wildcard', () => {
+    let myBluePencil = new BluePencil({wildcard: '######'});
+    const expectedObject = [{
+      root: [{
+        foo: '######',
+      }],
+    }];
+    const result = myBluePencil.censor([objectToCensor], valuesToHide);
+    expect(result).toStrictEqual(expectedObject);
+  });
+
+  it('given an object and no values to hide should return the given object', () => {
+    let myBluePencil = new BluePencil({wildcard: '######'});
+    const result = myBluePencil.censor(objectToCensor, []);
+    expect(result).toStrictEqual(objectToCensor);
   })
 });
